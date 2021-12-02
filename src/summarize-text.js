34e244de2119acd840cb4d25c82b5c93036be4e1
Matthew Conlen <mc@mathisonian.com>
2021-12-02 10:46:08 -0800

    Update summarize-text.js

--- src/summarize-text.js
+++ src/summarize-text.js
@@ -1,9 +1,7 @@
 
 const OpenAI = require('openai-api');
 
-// Load your key from an environment variable or secret management service
-// (do not include your key directly in your code)
-const OPENAI_API_KEY = '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';
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const openai = new OpenAI(OPENAI_API_KEY);
 
